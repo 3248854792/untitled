5eaef57a691cc4e1aae9e673d8e36168b002c18d
3248854792 <3248854792@qq.com>
2021-04-20 22:01:14 +0800

    Initial commit

---
+++ src/Village.java
@@ -0,0 +1,56 @@
+public class Village {
+    static int waterAmount;
+    int peopleNumber;
+    String name;
+    Village(String s) {
+        name = s;
+    }
+    static void setWaterAmount(int m) {
+        if(m>0)
+            waterAmount=m;
+    }
+    void drinkWater(int n) {
+        if(waterAmount-n>=0) {
+            waterAmount=waterAmount-n;
+            System.out.println(name+"喝了"+n+"升水");
+        }
+        else
+            waterAmount=0;
+    }
+    static int lookWaterAmount() {
+        return waterAmount;
+    }
+    void setPeopleNumber(int n) {
+        peopleNumber=n;
+    }
+    int getPeopleNumber() {
+        return peopleNumber;
+    }
+}
+class land {
+    public static void main(String args[]) {
+        Village.setWaterAmount(200);
+        int leftWater = Village.waterAmount;
+        System.out.println("水井中有" + leftWater + "升水");
+        Village zhaoZhuang, maJiaHeZhi;
+        zhaoZhuang = new Village("赵庄");
+        maJiaHeZhi = new Village("马家河子");
+        zhaoZhuang.setPeopleNumber(80);
+        maJiaHeZhi.setPeopleNumber(120);
+        zhaoZhuang.drinkWater(50);
+        leftWater=Village.lookWaterAmount();
+        String name=maJiaHeZhi.name;
+        System.out.println(name+"发现水井中有"+leftWater+"升水");
+        maJiaHeZhi.drinkWater(100);
+        leftWater=zhaoZhuang.lookWaterAmount();
+        name=zhaoZhuang.name;
+        System.out.println(name+"发现水井中有"+leftWater+"升水");
+        int peopleNumber=zhaoZhuang.getPeopleNumber();
+        System.out.println("赵庄的人口："+peopleNumber);
+        peopleNumber=maJiaHeZhi.getPeopleNumber();
+        System.out.println("马家河子的人口："+peopleNumber);
+
+
+
+    }
+}
